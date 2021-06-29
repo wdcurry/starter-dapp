5a98d4d26923d6b4a1993b19953235d1113d873f
drew curry <drew.curry@me.com>
2021-06-29 12:27:14 -0300

    update footer credit

--- react-delegationdashboard/src/components/Layout/Footer/index.jsx
+++ react-delegationdashboard/src/components/Layout/Footer/index.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer d-flex justify-content-center pb-spacer">
       <a target="_blank" rel="noreferrer" className="align-items-center" href="https://elrond.com/">
-        <small>Powered by Elrond Network</small>
+        <small>Powered by Elrond Network & ApeStaking</small>
       </a>
     </footer>
   );
